f532f57ffb4012a93d2bc78041fb0d6107706ba3
hamstercat007 <lisamac421@gmail.com>
2022-07-21 22:29:08 +0100

    plan for final success page

---
+++ success/index.js
@@ -0,0 +1,11 @@
+//congratulate on rebuilding planet Earth
+//they couldn't do it without you
+//you go back to space after training people with tech and leaving the processes behind
+//it was a battle, but your skills and perseverance paid off
+
+
+//what do you get out of it? 
+//your mission has been complete,  you served mother Earth and saved thousands of lives from the darkness of being void of technology. 
+
+//your next challenge 
+//go back and code another app. 
